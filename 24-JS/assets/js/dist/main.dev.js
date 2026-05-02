5a5db5e2c53d1beeddd32f796bc36591fc80e77c
JuliaChudo <chudo6686@gmail.com>
2026-05-02 09:51:31 +0300

    add exam

---
+++ 24-JS/assets/js/dist/main.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function yourName() {
+  var your_name = prompt("What is your name?");
+  /* 2name 
+  your-name 
+  name%first 
+  name|surname 
+  name/surname */
+}
+
+function helloName() {
+  var first_name = prompt("What is your name?");
+  alert("Hello, " + first_name);
+}
---
+++ 24-JS/assets/js/main.js
@@ -0,0 +1,16 @@
+function yourName(){
+const your_name = prompt("What is your name?")
+/* 2name 
+your-name 
+name%first 
+name|surname 
+name/surname */
+
+}
+function helloName(){
+    const first_name = prompt("What is your name?")
+    alert("Hello, " + first_name)
+}
+
+
+
